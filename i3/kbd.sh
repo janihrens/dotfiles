f2de355680b9ca0564691b88b7684dbed79a0996
Jan Ihrens <janihrens@gmail.com>
2016-12-08 01:58:44 +0100

    put keyboard layout command directly into i3block.conf

--- i3/kbd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# just a simple script to get the keyboard layout.
-# the return of the query doesnt seem to be tab separated
-# so: reverse, cut at first space and reverse again
-
-echo "$(setxkbmap -query | grep layout | rev | cut -d' ' -f1 | rev)"
