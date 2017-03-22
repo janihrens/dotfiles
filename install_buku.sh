a1e2dba4f1a4ef44285c339ebfa2a562f1cf8b28
Jan Ihrens <janihrens@gmail.com>
2017-03-22 03:41:09 +0100

    create db symlink to dropbox

--- install_buku.sh
+++ install_buku.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 BUKUDIR="$HOME/software/buku"
+BUKUDBDIR="$HOME/.local/share/buku/"
 BASHCOMPLETIONDIR="/etc/bash_completion.d/"
 ZSHCOMPLETIONDIR="$HOME/.oh-my-zsh/completions/"
+BUKUDB="$HOME/Dropbox/Sync/buku/bookmarks.db"
 mkdir -p $BUKUDIR
+mkdir -p $BUKUDBDIR
 mkdir -p $ZSHCOMPLETIONDIR
 
 
@@ -15,6 +18,8 @@ sudo apt-get install python3-urllib3 python3-cryptography python3-bs4 python3-re
 git clone https://github.com/jarun/Buku/ $BUKUDIR
 cd $BUKUDIR
 sudo make install
+rm $BUKUDBDIR/bookmarks.db
+ln -s $BUKUDB $BUKUDBDIR/bookmarks.db
 
 ### install autocompletion for shells
 cp $BUKUDIR/auto-completion/zsh/_buku $ZSHCOMPLETIONDIR
